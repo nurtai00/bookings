e26e21be159bb05ee9d669d2be31418dd8756208
Nurtay <nnn.nurtai00@gmail.com>
2021-11-30 21:55:05 +0500

    diploma

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"github.com/alexedwards/scs/v2"
 	"github.com/nurtai00/bookings/internal/config"
 	"github.com/nurtai00/bookings/internal/handlers"
+	"github.com/nurtai00/bookings/internal/models"
 	"github.com/nurtai00/bookings/internal/render"
 	"log"
 	"net/http"
@@ -18,6 +20,9 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 
